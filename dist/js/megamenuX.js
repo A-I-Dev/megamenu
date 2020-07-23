2c8d1b37357303c54c9ae24101197b18b9f776d8
A-I-Dev <aleksandar_ivanov@yahoo.com>
2020-07-23 12:43:08 +0300

    The menu now has two types of dropdown and is fully functional

--- dist/js/megamenuX.js
+++ dist/js/megamenuX.js
@@ -1,40 +1,128 @@
 (() => {
     const megaMenuParent = document.querySelector('.mega-menu-X');
-    const menuItems = [
-        {name: "Home", link: "/dist/index.html", mega: false},
-        {name: "Products", link: "", mega: true, children: [
-            {name: "Cheese", link: "/cheese", mega: true, children: [
-                {name: "Lamb", link: "/meat/soft", mega: false},
-                {name: "Goat", link: "/meat/hard", mega: false},
-                {name: "Cow", link: "/meat/fresh", mega: true, children: [
-                    {name: "Soft", link: "/meat/soft", mega: false},
-                    {name: "Hard", link: "/meat/hard", mega: false}              
-                ]}                
-            ]},
-            {name: "Eggs", link: "/eggs", mega: false},
-            {name: "Meat", link: "/meat", mega: true, children: [
-                {name: "Beef", link: "/meat/beef", mega: false},
-                {name: "Pork", link: "/meat/pork", mega: false},
-                {name: "Lamb", link: "/meat/lamb", mega: false}
-            ]}
-        ]}
+    let menuItems = [
+        {
+            name: "Home",
+            link: "index.html",
+            mega: false
+        },
+        {
+            name: "Products",
+            link: "",
+            mega: true,
+            withImages: true,
+            children: [
+                {
+                    name: "Cheese",
+                    img: "./img/meat.jpg",
+                    link: "/cheese",
+                    mega: true,
+                    children: [
+                        {
+                            name: "> Lamb",
+                            link: "/meat/soft",
+                            mega: false
+                        },
+                        {
+                            name: "> Goat",
+                            link: "/meat/hard",
+                            mega: false
+                        },
+                        {
+                            name: "> Cow",
+                            link: "/meat/fresh",
+                            mega: false,
+                            // children: [
+                            //     {
+                            //         name: "Soft",
+                            //         link: "/meat/soft",
+                            //         mega: false
+                            //     },
+                            //     {
+                            //         name: "Hard",
+                            //         img: "./img/meat.jpg",
+                            //         link: "/meat/hard",
+                            //         mega: false
+                            //     }
+                            // ]    
+                        }
+                    ]
+                },
+                {
+                    name: "Eggs",
+                    img: "./img/meat.jpg",
+                    link: "/eggs",
+                    mega: false
+                },
+                {
+                    name: "Meat",
+                    img: "./img/meat.jpg",
+                    link: "/meat",
+                    mega: true,
+                    children: [
+                        {
+                            name: "> Beef",
+                            link: "/meat/beef",
+                            mega: false
+                        },
+                        {
+                            name: "> Pork",
+                            link: "/meat/pork",
+                            mega: false
+                        },
+                        {
+                            name: "> Lamb",
+                            link: "/meat/lamb",
+                            mega: false
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Manufacturers',
+            link: '/manufacturers',
+            mega: true,
+            children: [
+                {
+                    name: 'Vereia',
+                    link: '/vereia',
+                    mega: false
+                },
+                {
+                    name: 'Milbona',
+                    link: '/vereia',
+                    mega: false
+                },
+                {
+                    name: 'Pilos',
+                    link: '/vereia',
+                    mega: false
+                }
+            ]
+        }
     ];
 
-    menuItems.forEach(item => {
-        let itemMarkup = `
-                        <li class="mm-item mega">
-                            <a class="mm-item-text" href="${item.link}">${item.name}</a>
-                            ${item.mega ? 
-                                `<ul class="mm-sub-menu">
-                                                ${prepareSubMenus(item.children)}
-                                            </ul>` : 
-                                ``
-                            }
-                        </li>
-                        `;
+    prepareMainMenu(menuItems)    
 
-        megaMenuParent.insertAdjacentHTML('beforeend', itemMarkup);
-    })
+    function prepareMainMenu(menuItemsJSON) {
+        const menuItemsArray = menuItemsJSON;
+        menuItems.forEach(item => {
+            let itemMarkup = `
+                            <li class="mm-item ${item.mega ? 'mega' : ''}">
+                                <a class="mm-item-text" href="${item.link}">${item.name}</a>
+                                ${item.mega ? 
+                                    `<ul class="mm-sub-menu ${item.withImages ? 'with-images' : ''}">
+                                                    ${prepareSubMenus(item.children)}
+                                                </ul>` : 
+                                    ``
+                                }
+                            </li>
+                            `;
+    
+            megaMenuParent.insertAdjacentHTML('beforeend', itemMarkup);
+        })
+    }
 
     function prepareSubMenus(menuItemChildren) {
         const menuItemObject = menuItemChildren;
@@ -44,6 +132,14 @@
             itemMarkup += 
             `<li class="sm-item">
                 <a class="sm-item-text" href="${child.link}">${child.name}</a>
+                ${child.img ? 
+                    `
+                    <a class="img-wrap" href="${child.link}">
+                        <img src="${child.img}">
+                    </a>
+                    ` :
+                    ``
+                }
                 ${child.mega ? 
                     `<ul class="mm-sub-menu">
                                     ${prepareSubMenus(child.children)}
@@ -56,3 +152,8 @@
         return itemMarkup;
     }
 })()
+
+    // fetch('http://127.0.0.1:5500/dist/js/menuItems.json')
+    //     .then(res => res.json())
+    //     .then(data => menuItems = data)
+    //     .then(() => prepareMainMenu(menuItems));
